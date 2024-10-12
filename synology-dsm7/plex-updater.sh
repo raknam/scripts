d0ab8e6798127fb6f400eb0e90664676f9f918cc
Sylvain <raknam@users.noreply.github.com>
2024-10-12 19:13:27 +0200

    Update plex-updater.sh

--- synology-dsm7/plex-updater.sh
+++ synology-dsm7/plex-updater.sh
@@ -8,12 +8,13 @@
 # https://github.com/martinorob/plexupdate/
 
 PLEX_PKG_NAME=PlexMediaServer
+PLEX_VOLUME=/volume2
 
 mkdir -p /tmp/plex/ > /dev/null 2>&1
-PLEX_TOKEN=$(cat /volume4/PlexMediaServer/AppData/Plex\ Media\ Server/Preferences.xml | grep -oP 'PlexOnlineToken="\K[^"]+')
+PLEX_TOKEN=$(cat $PLEX_VOLUME/PlexMediaServer/AppData/Plex\ Media\ Server/Preferences.xml | grep -oP 'PlexOnlineToken="\K[^"]+')
 PLEX_URL=$(echo "https://plex.tv/api/downloads/5.json?channel=plexpass&X-Plex-Token=$PLEX_TOKEN")
 PLEX_JSON=$(curl -s "$PLEX_URL")
-PLEX_NEW_VER=$(echo "$PLEX_JSON" | jq -r .nas.Synology.version | awk -F- '{print $1}')
+PLEX_NEW_VER=$(echo "$PLEX_JSON" | jq -r '.nas."Synology (DSM 7)".version' | awk -F- '{print $1}')
 echo "New Ver: $PLEX_NEW_VER"
 PLEX_CUR_VER=$(synopkg version $PLEX_PKG_NAME| awk -F- '{print $1}')
 echo "Cur Ver: $PLEX_CUR_VER"
